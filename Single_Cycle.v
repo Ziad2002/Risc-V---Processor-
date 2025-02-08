24e7facc767bd12f892574df70327e23356e5e74
Ziad Mahmoud Elbadry Shaker Jr <zes5045@moore.wot.eecs.northwestern.edu>
2025-02-07 19:59:05 -0600

    add memory

--- Single_Cycle.v
+++ Single_Cycle.v
@@ -1,3 +1,10 @@
+module SingleCycleCPU(
+    input clk, reset,
+    output halt
+);
+
+endmodule
+
 // Program Counter
 module Program_Counter(
     input clk, reset,
@@ -16,10 +23,10 @@ endmodule
 
 // PC + 4
 module PCplus4(
-    input [31:0] fromPC, 
-    output [31:0] NextoPC
+    input [31:0] PC, 
+    output [31:0] PC_Plus_4
     );
-    assign NextoPC = fromPC + 4;
+    assign PC_Plus_4 = PC + 4;
 endmodule
 
 // Instruction Memory
@@ -29,17 +36,17 @@ module Instruction_Memory(
     output [31:0] instruction_out
 );
 
-reg [7:0] IMEM [0:1024];
+reg [7:0] IMem [0:1024];
 wire [31:0] InstAddr;
 integer i;
 
 assign InstAddr = read_addresss & 32'hfffffffc; // word alignment
-assign instruction_out = {IMEM[InstAddr+3], IMEM[InstAddr+2], IMEM[InstAddr+1], IMEM[InstAddr]};
+assign instruction_out = {IMem[InstAddr+3], IMem[InstAddr+2], IMem[InstAddr+1], IMem[InstAddr]};
 
 always @(posedge clk) begin
     if(reset) begin
         for (i = 0; i < 1024; i = i + 4) begin
-            {IMEM[i+3], IMEM[i+2], IMEM[i+1], IMEM[i]} <= 32'b0;  // Reset all memory locations
+            {IMem[i+3], IMem[i+2], IMem[i+1], IMem[i]} <= 32'b0;  // Reset all memory locations
         end
     end
 end
@@ -109,6 +116,7 @@ always @(*) begin
 end 
 endmodule
 
+// ALU UNIT
 module ALU_unit(
     input [31:0] A, B, 
     input [3:0] Control_in, 
@@ -125,8 +133,112 @@ always @(*) begin
     4'b0100 : begin if(A==B) zero <= 1; else zero <= 0; ALU_Result <= A - B; end // beq
     4'b0101 : begin if(A<B) zero <= 1; else zero <= 0; ALU_Result <= A - B; end // blt
     4'b0110 : begin if(A>=B) zero <= 1; else zero <= 0; ALU_Result <= A - B; end // bge
-    4'b0111 : begin zero <= 0; ALU_Result <= A - B; end // jal
+    4'b0111 : begin zero <= 1; ALU_Result <= A - B; end // jal
     endcase 
 end 
 
 endmodule
+
+// ALU CONTROL
+module ALU_Contorl( 
+    input [6:0] opcode,
+    input [6:0] fun7, 
+    input [2:0] fun3, 
+    output reg [3:0] Control_out
+);
+
+always @(*) begin
+casex ({opcode, fun3, fun7})
+    17'b0110011_000_0000000 : Control_out <= 4'b0000; // ADD
+    17'b0010011_000_xxxxxxx : Control_out <= 4'b0000; // ADDI
+    17'b0110011_000_0100000 : Control_out <= 4'b0001; // SUB
+    17'b0110011_000_0000001 : Control_out <= 4'b0010; // MUL
+    17'b0110011_100_0100001 : Control_out <= 4'b0011; // DIV
+    17'b1100011_000_xxxxxxx : Control_out <= 4'b0100; // beg
+    17'b1100011_100_xxxxxxx : Control_out <= 4'b0101; // blt
+    17'b1100011_101_xxxxxxx : Control_out <= 4'b0110; // bge
+    17'b1101111_xxx_xxxxxxx : Control_out <= 4'b0111; // jal
+endcase
+end
+endmodule
+
+// Data Memory
+module Data_Memory(
+    input clk, reset, MemWrite, MemRead, 
+    input [31:0] DataAddr, WriteData, 
+    output [31:0] ReadData
+);
+
+reg [7:0] 	Mem[0:1024];
+wire [31:0] DataAddrW;
+integer i;
+
+assign DataAddrW = DataAddr & 32'hfffffffc;
+assign ReadData = (MemRead) ? {Mem[DataAddrW+3], Mem[DataAddrW+2], Mem[DataAddrW+1], Mem[DataAddrW]} : 32'b00;
+
+
+always @(posedge clk) begin 
+    if (reset) begin 
+        for(i=0; i <1024; i=i+1) begin 
+            Mem[i] <= 8'b00;
+        end
+    end else if(MemWrite) begin
+        Mem[DataAddrW] <= WriteData[7:0];
+        Mem[DataAddrW+1] <= WriteData[15:8];
+        Mem[DataAddrW+2] <= WriteData[23:16];
+        Mem[DataAddrW+3] <= WriteData[31:24];
+    end 
+end 
+endmodule
+
+
+// Mux1
+
+module Mux1(
+    input sel, 
+    input [31:0] A, B,
+    output [31:0] Mux_out
+);
+
+assign Mux_out = (sel==1'b0) ? A : B;
+
+endmodule
+
+module Mux2(
+    input sel2, 
+    input [31:0] A2, B2,
+    output [31:0] Mux_out2
+);
+
+assign Mux_out2 = (sel2==1'b0) ? A2 : B2;
+
+endmodule
+
+module Mux3(
+    input sel3, 
+    input [31:0] A3, B3,
+    output [31:0] Mux_out3
+);
+
+assign Mux_out3 = (sel3==1'b0) ? A3 : B3;
+
+endmodule
+
+module And_logic(
+    input zero, branch,
+    output and_out
+);
+
+assign and_out = zero & branch;
+
+endmodule
+
+module Adder(
+    input [31:0] in_1, in_2,
+    output [31:0] Sum_out
+);
+
+assign Sum_out = in_1 + in_2;
+
+endmodule
+
